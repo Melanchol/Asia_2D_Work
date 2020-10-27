c94dccc8ab57f18f2c43425ce8b1a9eef048992d
Melanchol <71679869+Melanchol@users.noreply.github.com>
2020-10-27 16:00:12 +0800

    加上傳送門&目錄腳本
    我的門門是菱形的喔:D

---
+++ Asia_2D_Work/Asia_2D_Work/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // 按鈕要與程式溝通的橋樑=方法(函式/功能)
+    //語法:類型 方法名稱 () {}
+    void StartGame()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void EndGame()
+    {
+        
+    }
+}
